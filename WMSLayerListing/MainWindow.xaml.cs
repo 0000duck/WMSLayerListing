b79776d659555e90731bcd5e5d65ae57671f5380
Sachindra Singh <sachindra@sopac.org>
2012-09-25 15:29:24 +1200

    Initial commit WMS GetCapabilities Layer Listing.

---
+++ WMSLayerListing/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WMSLayerListing
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WMSParser parser = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            parser = new WMSParser();
+        }
+
+
+
+        private void btnParse_Click(object sender, RoutedEventArgs e)
+        {
+            list.Items.Clear();
+            var res = parser.getLayers(textURL.Text);
+            textStatus.Text = res.Count.ToString() + " Layers Parsed.";
+            foreach (String layer in res)
+            {
+                list.Items.Add(layer);
+            }
+
+        }
+
+       
+    }
+}
---
+++ WMSLayerListing/WMSParser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Xml;
+
+namespace WMSLayerListing
+{
+    class WMSParser
+    {
+
+        //constructor
+       public WMSParser()
+        {
+            Console.WriteLine("Initialized");
+        }
+        
+        public String xmlCache = null;
+        public bool initialLoad = false;
+
+        public List<String> getLayers(String url)
+        {
+            //cache GetCapabilites document
+            if (xmlCache == null)
+            {
+                using (WebClient client = new WebClient())
+                {
+                    initialLoad = true;
+                    xmlCache = client.DownloadString(url);
+                }
+            }
+            
+            //parse using DOM (consider using SAX - XMLReader class or XML/LINQ XDocument class for better performance) 
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(xmlCache);
+            var layers = doc.GetElementsByTagName("Layer");
+
+            var resList = new List<String>();
+            foreach (XmlElement layer in layers)
+            {
+                if (layer.HasAttributes)
+                {
+                    //get layer title
+                    resList.Add(layer.GetElementsByTagName("Title")[0].InnerText);
+                    //get layer name
+                    //resList.Add(layer.GetElementsByTagName("Name")[0].InnerText);
+                }
+            }
+
+            if (initialLoad)
+            {
+                initialLoad = false;
+                Console.WriteLine(layers.Count.ToString() + " Layers Parsed.");
+                //System.Windows.MessageBox.Show(layers.Count.ToString() + " Layers Parsed.");
+            }
+            return resList;
+        }
+    }
+}
